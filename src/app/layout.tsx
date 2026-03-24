1e7663bdc9ea3aa95716f5d495295227934c7c2f
Hamilton Vinicius <hamilton.vinicius@gmail.com>
2026-03-24 12:36:08 -0300

    feat: adicionar Pixel do Facebook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { QuoteCartProvider } from '@/contexts/quote-cart-context'
 import ClientLayout from '@/components/layout/client-layout'
@@ -26,6 +27,23 @@ export default function RootLayout({
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=block" />
             </head>
             <body className={`${jakarta.variable} font-display bg-background-light dark:bg-background-dark text-slate-900 dark:text-slate-100 antialiased pb-16 md:pb-0`}>
+                <Script id="facebook-pixel" strategy="afterInteractive">
+                    {`
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '3970899879684698');
+                        fbq('track', 'PageView');
+                    `}
+                </Script>
+                <noscript>
+                    <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=3970899879684698&ev=PageView&noscript=1" alt="" />
+                </noscript>
                 <QuoteCartProvider>
                     <ClientLayout>
                         {children}
